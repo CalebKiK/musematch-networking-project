d739b34285825827a11301bb81022053617ac93e
CalebKiK <karimicaleb@gmail.com>
2024-12-07 11:59:53 +0300

    Website responsive

--- README.md
+++ README.md
@@ -8,18 +8,18 @@ For the reasons above, this website aims to enable design professionals easily g
 
 This has been achieved in the following ways:
 
-The professionals can create accounts through pop up forms and update their accounts with relevant information such as their profession, the services they offer and provide images to show their past works.
+* The professionals can create accounts through pop up forms and update their accounts with relevant information such as their profession, the services they offer and provide images to show their past works.
 
-The users can signup or log in through forms as well and have personal accounts where they can scroll through various professional profiles to find their designer match.
+* The users can signup or log in through forms as well and have personal accounts where they can scroll through various professional profiles to find their designer match.
 
-There is a chat feature to further improve communication between various users to enhance conversations.
+* There is a chat feature to further improve communication between various users to enhance conversations.
 
-The website provides search bars at specific areas to allow users to search for or filter out information such as professionals, specific project types and even particular jobs.
+* The website provides search bars at specific areas to allow users to search for or filter out information such as professionals, specific project types and even particular jobs.
 
-There is an educational section whereby users can gain access to educational resources so as to acquire knowledge on specific topics they desire to know more about.
+* There is an educational section whereby users can gain access to educational resources so as to acquire knowledge on specific topics they desire to know more about.
 
-Users can also get in touch at the bottom of the page with the website admin in case of any inquiries.
+* Users can also get in touch at the bottom of the page with the website admin in case of any inquiries.
 
-Lastly, the users can input information into the website dynamically through various popup forms that serve different purposes such as: posting jobs, updating their profile and upload educational content.
+* Lastly, the users can input information into the website dynamically through various popup forms that serve different purposes such as: posting jobs, updating their profile and upload educational content.
 
 The information from the pop up forms are displayed at their respective sections on the website and those who have posted information are able to manipulate the information through editing and deleting if they desire.
--- index.js
+++ index.js
@@ -12,13 +12,291 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const viewProfileBtns = document.querySelectorAll(".view-profile-btn");
 
+  const userAuthenticationContainer = document.getElementById("user-authentication")
   const signupForm = document.getElementById("signup-form");
   const signupBtn = document.querySelector(".signup-btn");
+  const loginForm = document.getElementById("logIn-form");
+  const userProfileBtn = document.getElementById("user-profile-btn");
   let signupPassword = document.getElementById("signup-password").value;
 
-  const profileModal = document.getElementById("professional-profile-modal");
+  const userProfileInfo = document.getElementById("professional-profile-modal");
   const profileModalContent = document.getElementById("modal-content");
   const updatePortfolio = document.getElementById("add-content-portfolio");
+
+  const websiteContent = document.getElementById("website-content");
+
+  const hamburgerIcon = document.getElementById('hamburger-icon');
+  const menuContainer = document.querySelector('.menu-container');
+
+  hamburgerIcon.addEventListener('click', () => {
+    menuContainer.classList.toggle('open');
+  });
+
+  // Function to display signup form when button is clicked on
+  function displayUserAuthForm(event) {
+    event.preventDefault();
+    signupForm.style.display = "flex";
+    loginForm.style.display = "none";
+
+    const closeUserAuthForm = signupForm.querySelector(".btn-close");
+    closeUserAuthForm.addEventListener("click", () => {
+      userAuthenticationContainer.style.display = "none";
+    });
+  }
+
+  document.querySelector(".signup-btn").addEventListener("click", displayUserAuthForm);
+  document.querySelector("#signin-btn").addEventListener("click", displayLoginForm);
+  
+  document.querySelector("#signup-redirect-btn").addEventListener("click", () => {
+    signupForm.style.display = "flex";
+    loginForm.style.display = "none";
+  });
+
+  // Function to display login form when button is clicked on
+  function displayLoginForm(event){
+    event.preventDefault();
+    loginForm.style.display = "flex";
+    signupForm.style.display = "none";
+
+    const closeLoginForm = loginForm.querySelector(".btn-close");
+    closeLoginForm.addEventListener("click", () => {
+      userAuthenticationContainer.style.display = "none";
+    });
+  };
+
+  async function fetchProfessionals() {
+    const response = await fetch('http://localhost:3000/professionals');
+    const professionals = await response.json();
+    return professionals;
+};
+
+async function fetchCurrentUser() {
+  const response = await fetch('http://localhost:3000/currentUser');
+  const currentUser = await response.json();
+  return currentUser;
+};
+
+async function updateUI() {
+  const currentUser = await fetchCurrentUser();
+  if (currentUser) {
+      userAuthenticationContainer.style.display = 'none';
+      signupBtn.style.display = "none"
+      websiteContent.style.display = 'block';
+      userProfileInfo.style.display = 'none';
+      userProfileInfo.innerText = `
+      <div class="profile-modal-content">
+          <h4>Your Profile</h4>
+          <button type="button" class="btn-close" id="btn-profile-close" aria-label="Close"></button>
+          <div id="professional-profile-modal">
+            <div id="profile-prt1">
+              <img src="${currentUser.profileImage}" alt="">
+              <div class="profile-text">
+                <h4>${currentUser.signupName}</h4>
+                <h5>${currentUser.userProfession}</h5>
+                <p>${currentUser.signupServices}</p>
+              </div>
+              <button class = "edit-profile-btn">Edit Profile</button>
+              <button class = "delete-profile-btn">Delete profile</button>
+            </div>
+            <div id="profile-prt2">
+              <div class="profile-portfolio">
+                <img src="Images/Projects/concept render 2.jpg" alt="">
+                <h5>Mixed use developemnt project</h5>
+                <p>This type of development promotes a vibrant, sustainable community by providing residents and visitors with a convenient blend of living, working, shopping, and leisure opportunities in one location. </p>
+              </div>
+              <div class="profile-portfolio">
+                <img src="Images/Projects/Interior side.jpg" alt="">
+                <h5>Affordable housing project</h5>
+                <p>A residential building designed to provide comfortable, modern living spaces for individuals and families. This project features a variety of unit types, from cozy studios to spacious multi-bedroom apartments, all equipped with high-quality finishes and contemporary amenities. </p>
+              </div>
+            </div> 
+            <button class = "logout-btn">Logout</button>
+          </div>
+        </div>
+      `;
+  } else {
+      userAuthenticationContainer.style.display = 'block';
+      websiteContent.style.display = 'none';
+      userProfileInfo.style.display = 'none';
+  }
+};
+
+  // Function to submit signup details to the server
+  function signupFormSubmit(event) {
+    event.preventDefault();
+
+    let signupName = document.getElementById("signup-name").value.trim();
+    let signupEmail = document.getElementById("signup-email").value.trim();
+    let profileImage = document.getElementById("signup-profile-photo").value.trim();
+    let userProfession = document.getElementById("signup-profession").value.trim();
+    let signupServices = document.getElementById("signup-services").value.trim();
+    let signupPassword = document.getElementById("signup-password").value.trim();
+    let signupReEnterPassword = document.getElementById("signup-reEnter-password").value.trim();
+
+    if (!signupEmail || !signupPassword || !profileImage || !signupName) {
+      alert("Please fill in all required fields.");
+      return;
+    }
+
+    async function checkUserName(signupName) {
+      try {
+        const response = await fetch("http://localhost:3000/professionals");
+    
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+    
+        const professionalsData = await response.json();
+    
+        const existingUser = professionalsData.find(professional => professional.signupName === signupName);
+    
+        if (existingUser) {
+          alert(`${signupName} already exists`);
+        } else {
+          alert(`${signupName} is available`);
+        }
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    }
+    checkUserName();
+
+    let newUser = {
+      profileImage,
+      signupName,
+      userProfession,
+      signupServices,
+      signupEmail,
+      signupPassword,
+      portfolioImages: []
+    };
+
+    fetch("http://localhost:3000/professionals", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        
+        signupForm.style.display = "none",
+        signupBtn.style.display = "none";
+
+        const profileBtn = document.createElement("button");
+        profileBtn.classList.add("profile-btn");
+        profileBtn.textContent = "Your Profile";
+        document.querySelector("header").appendChild(profileBtn);
+
+        profileBtn.addEventListener("click", () => {
+          showUserProfile(data);
+        })  
+        
+      })
+      .catch((error) => console.error("Error signing up:", error));
+  }
+  signupForm.addEventListener("submit", signupFormSubmit);
+
+  // Function to use login form
+  async function loginFormSubmit(event) {
+    event.preventDefault();
+
+    let loginEmail = document.getElementById("login-email").value;
+    let loginPassword = document.getElementById("login-password").value;
+
+    let professionals = await fetchProfessionals();
+    let professional = professionals.find(professional => professional.signupEmail === loginEmail)
+    if(!professional){
+      alert("No email found. Please check")
+      return;
+    }
+
+    if(professional.signupPassword === loginPassword) {
+      await fetch('http://localhost:3000/currentUser', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(professional),
+      });
+      updateUI();
+    } else {
+      alert('Invalid password.');
+    }
+  };
+  loginForm.addEventListener("submit", loginFormSubmit);
+
+  // Function to show the profile of user
+  function showUserProfile(user) {
+    profileModal.style.display = "block";
+
+    let addToPorfolioBtn = document.createElement("button");
+    addToPorfolioBtn.textContent = "Add to portfolio";
+    addToPorfolioBtn.className = "add-portfolio-btn";
+
+    let updateProfileBtn = document.createElement("button");
+    updateProfileBtn.textContent = "Update profile";
+    updateProfileBtn.className = "edit-profile-btn";
+
+    let deleteAccountBtn = document.createElement("button");
+    deleteAccountBtn.textContent = "Delete Account";
+    deleteAccountBtn.className = "delete-profile-btn";
+
+    const profileModal = document.createElement("div");
+    modalClose.classList.add("profile-modal");
+
+    // let profileItems = "";
+    //   profileItems += `
+    //     <div class="portfolio-item">
+    //     <img src="${user.imageUrl}" alt="${user.projectTitle}">
+    //       <h5>${user.projectTitle}</h5>
+    //       <p>${user.projectDescription}</p>
+    //     </div>`;
+    // ;
+
+    profileModal.innerHTML = `
+    <div class="profile-modal-content">
+      <button type="button" class="btn-close" aria-label="Close"></button>
+      <div id="profile-prt1">
+        <img src="${user.profileImage}" alt="${user.signupName}">
+        <div class="profile-text">
+          <h4>${user.professionalName}</h4>
+          <h5>${user.profession}</h5>
+          <p>${user.professionalServices}</p>
+        </div>
+        <div class="profile-portfolio">
+          ${user.portfolioImages.map(image => `
+            <div class="portfolio-profile-item">
+            <img src="${image.imageUrl}" alt="${image.projectTitle}">
+            <h5>${image.projectTitle}</h5>
+            <p>${image.projectDescription}</p>
+        </div>
+        `).join('')}
+        </div>
+
+    </div>
+      `;
+      
+      document.body.appendChild(profileModal);
+
+      profileModal.appendChild(addToPorfolioBtn);
+      profileModal.appendChild(updateProfileBtn);
+      profileModal.appendChild(deleteAccountBtn);
+
+      
+
+      modal.querySelector('.close').addEventListener('click', () => {
+        document.body.removeChild(modal);
+    });
+  }
+
+  document.getElementById("user-profile-btn").addEventListener("click", showUserProfile);
+
+  // document.getElementById("signup-profile-photo").addEventListener("click", function() {
+  //   showUserProfile({
+  //     profileImage: this.value,
+  //     signupName: document.getElementById("signup-name").value,
+  //     userProfession: document.getElementById("signup-profession").value,
+  //     signupServices: document.getElementById("signup-services").value
+  //   });
+  // });
 
 // Function to GET the professionals profile
   function fetchProfessionalProfile() {
@@ -166,18 +444,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
   document.querySelectorAll(".post-job-btn").forEach((button) => {button.addEventListener("click", displayPostJobForm);});
-
-  // Function to display signup/login form when button is clicked on
-  function displaySignupForm() {
-    signupForm.style.display = "flex";
-
-    const closeSignupForm = signupForm.querySelector(".btn-close");
-    closeSignupForm.addEventListener("click", () => {
-      signupForm.style.display = "none";
-    });
-  }
-
-  document.querySelector(".signup-btn").addEventListener("click", displaySignupForm);
 
 // Function to display portfolio form
   function displayAddToPortfolioForm() {
@@ -389,139 +655,6 @@ function deleteArticle(articleId) {
   }
 
   postJobForm.addEventListener("submit", postJobFormSubmit);
-  
-// Function to submit signup details to the server
-  function signupFormSubmit(event) {
-    event.preventDefault();
-
-    let signupName = document.getElementById("signup-name").value.trim();
-    let signupEmail = document.getElementById("signup-email").value.trim();
-    let profileImage = document.getElementById("signup-profile-photo").value.trim();
-    let userProfession = document.getElementById("signup-profession").value.trim();
-    let signupServices = document.getElementById("signup-services").value.trim();
-    let signupPassword = document.getElementById("signup-password").value.trim();
-    let signupReEnterPassword = document.getElementById("signup-reEnter-password").value.trim();
-
-    if (!signupEmail || !signupPassword || !profileImage || !signupName) {
-      alert("Please fill in all required fields.");
-      return;
-    }
-
-    let newUser = {
-      profileImage,
-      signupName,
-      userProfession,
-      signupServices,
-      signupEmail,
-      signupPassword,
-      portfolioImages: []
-    };
-
-    fetch("http://localhost:3000/professionals", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(newUser),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        
-        signupForm.style.display = "none",
-        signupBtn.style.display = "none";
-
-        const profileBtn = document.createElement("button");
-        profileBtn.classList.add("profile-btn");
-        profileBtn.textContent = "Your Profile";
-        document.querySelector("header").appendChild(profileBtn);
-
-        profileBtn.addEventListener("click", () => {
-          showUserProfile(data);
-        })  
-        
-      })
-      .catch((error) => console.error("Error signing up:", error));
-  }
-  signupForm.addEventListener("submit", signupFormSubmit);
-
-// Function to show the profile of user
-  function showUserProfile(user) {
-    profileModal.style.display = "block";
-
-    let addToPorfolioBtn = document.createElement("button");
-    addToPorfolioBtn.textContent = "Add to portfolio";
-    addToPorfolioBtn.className = "add-portfolio-btn";
-
-    let updateProfileBtn = document.createElement("button");
-    updateProfileBtn.textContent = "Update profile";
-    updateProfileBtn.className = "edit-profile-btn";
-
-    let deleteAccountBtn = document.createElement("button");
-    deleteAccountBtn.textContent = "Delete Account";
-    deleteAccountBtn.className = "delete-profile-btn";
-
-    const profileModal = document.createElement("div");
-    modalClose.classList.add("profile-modal");
-
-    // let profileItems = "";
-    //   profileItems += `
-    //     <div class="portfolio-item">
-    //     <img src="${user.imageUrl}" alt="${user.projectTitle}">
-    //       <h5>${user.projectTitle}</h5>
-    //       <p>${user.projectDescription}</p>
-    //     </div>`;
-    // ;
-
-    profileModal.innerHTML = `
-    <div class="profile-modal-content">
-      <button type="button" class="btn-close" aria-label="Close"></button>
-      <div id="profile-prt1">
-        <img src="${user.profileImage}" alt="${user.signupName}">
-        <div class="profile-text">
-          <h4>${user.professionalName}</h4>
-          <h5>${user.profession}</h5>
-          <p>${user.professionalServices}</p>
-        </div>
-        <div class="profile-portfolio">
-          ${user.portfolioImages.map(image => `
-            <div class="portfolio-profile-item">
-            <img src="${image.imageUrl}" alt="${image.projectTitle}">
-            <h5>${image.projectTitle}</h5>
-            <p>${image.projectDescription}</p>
-        </div>
-        `).join('')}
-        </div>
-
-    </div>
-      `;
-      
-      document.body.appendChild(profileModal);
-
-      profileModal.appendChild(addToPorfolioBtn);
-      profileModal.appendChild(updateProfileBtn);
-      profileModal.appendChild(deleteAccountBtn);
-
-      
-
-      modal.querySelector('.close').addEventListener('click', () => {
-        document.body.removeChild(modal);
-    });
-  }
-
-  document.getElementById("signup-profile-photo").addEventListener("click", function() {
-    showUserProfile({
-      profileImage: this.value,
-      signupName: document.getElementById("signup-name").value,
-      userProfession: document.getElementById("signup-profession").value,
-      signupServices: document.getElementById("signup-services").value
-    });
-  });
-    
-// Function to use login form
-  function loginFormSubmit(event) {
-    event.preventDefault();
-
-    let loginEmail = document.getElementById("login-email");
-    let loginPassword = document.getElementById("login-password");
-  }
 
   // Function to submit update portfolio form
   function updatePortfolioFormSubmit(event) {
